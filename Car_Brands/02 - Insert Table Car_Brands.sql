2dd32bdc84d5f7e1dfff7d891d992c1ada429825
vulcanobr <vulcanorjbr@gmail.com>
2022-12-03 23:48:31 -0300

    Create, Insert Table Car_Brands

---
+++ Car_Brands/02 - Insert Table Car_Brands.sql
@@ -0,0 +1,14 @@
+-- SQLite
+-- Insere dados na Tabela CAR_BRANDS
+INSERT INTO CAR_BRANDS (BRAND_NAME)
+    VALUES  ('Chevrolet'),
+ 	        ('Toyota'),
+ 	        ('Hyundai'),
+ 	        ('Volkswagen'),
+	        ('Jeep'),
+ 	        ('Renault'),
+ 	        ('Honda'),
+ 	        ('Fiat');
+
+
+SELECT * FROM CAR_BRANDS;
---
+++ Car_Models/02 - Insert Table Car_Models.sql
@@ -0,0 +1,16 @@
+-- SQLite
+-- Insere dados na Tabela CAR_MODELS
+INSERT INTO CAR_MODELS (MODEL_NAME)
+VALUES  ('Conversível'),
+ 	    ('Sedã'),
+ 	    ('Hatch'),
+ 	    ('Coupé'),
+ 	    ('Perua'),
+ 	    ('SUV'),
+ 	    ('Picape'),
+ 	    ('Minivan'),
+ 	    ('Utilitário'),
+ 	    ('Buggy');
+
+
+SELECT * FROM CAR_MODELS;
