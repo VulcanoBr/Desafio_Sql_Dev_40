8325ffa140c1efa3c7d25652a096b325dd79b946
vulcanobr <vulcanorjbr@gmail.com>
2022-12-04 00:25:59 -0300

    Create, Insert Table Employees

---
+++ Positions/02 - Insert Table Positions.sql
@@ -0,0 +1,11 @@
+-- SQLite
+-- Insere dados Tabela POSITIONS
+INSERT INTO POSITIONS (DESCRIPTION)
+    VALUES  ('Gerente de vendas'),
+ 	        ('Gerente de compras'),
+ 	        ('Vendedor'),
+ 	        ('Mecânico'),
+ 	        ('Assistente Administrativo');
+
+
+SELECT * FROM POSITIONS;
